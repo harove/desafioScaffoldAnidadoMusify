e65edc22eca7645011db8e5580c346f38a751d17
edgar <harove@gmail.com>
2019-06-06 12:25:47 -0400

    rutas anidadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :playlists
+  resources :playlists do
+    post 'songs', to: 'songs#create'
+  end
+
+
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
